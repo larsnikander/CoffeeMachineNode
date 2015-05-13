b32af4c352e3fa3f3e75d25a51302a754c2ff112
root <root@raspberrypi.(none)>
2015-05-13 15:13:47 +0200

    install.sh no longer makes the config.properties

--- install.sh
+++ install.sh
@@ -2,17 +2,13 @@ startInstallation(){
 	echo "ok, starting installation. (this may take some time) "
 	apt-get install npm nodejs
 	npm update --save
-	echo "What should the name of this machine be? "
-	read name
-	echo "okay, the name of this machine is now $name"
-	echo "What should the subtitle be? "
-	read subtitle
-	echo "okay, the subtitle is now: $subtitle"
-	echo "{\"name\":\"$name\",\"subtitle\":\"$subtitle\"}" > config.properties
 	nohup node machine.js > output.txt &
 	echo "Please go to: "
 	ifconfig eth0 | awk '/inet addr/{print substr($2,6)}' | awk '{print $0"/config"}'
-	echo "And fill in the rest of the information."
+	echo "And configure the information you want."
+	echo "If you want to add or configure sensors, go to: "
+	ifconfig eth0 | awk '/inet addr/{print substr($2,6)}' | awk '{print $0"/addSensor"}'
+	echo "Please note that you have to be on the same network, to access the machines ip. "
 }
 
 
